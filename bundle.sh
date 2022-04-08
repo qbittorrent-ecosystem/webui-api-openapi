6d1a509e7b5e4d09d0c23df4a4d98ed54b6fa23c
ultram4rine <ultramarine730@gmail.com>
2022-04-08 17:06:25 +0400

    add dry-run flag to bundle to only validate specs

--- bundle.sh
+++ bundle.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-set -eu
+set -e
 
 for d in specs/v2*; do
     if [ -d "$d" ]; then
         npm run validate --dir="$d"
-        npm run bundle --dir="$d"
+        if [ "$1" != "--dry-run" ]; then
+            npm run bundle --dir="$d"
+        fi
     fi
 done
